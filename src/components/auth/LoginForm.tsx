d1a10a901af52407d1dad89789e892fc1139de5f
Hayeon-Lee <papepopepe@naver.com>
2025-03-13 00:24:29 +0900

    feat: implement custum auth with Firebase (nickname/password)

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import loginUser from '../../services/authService';
 
 const LoginForm = () => {
   const [nickname, setNickName] = useState<string>('');
   const [password, setPassWord] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError('');
+    setSuccess(false);
+
+    try {
+      const userData = await loginUser(nickname, password);
+      console.log('로그인 성공: ', userData);
+      setSuccess(true);
+    } catch (err) {
+      setError((err as Error).message);
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleLogin}>
       <input
         type="text"
         placeholder="오픈 채팅방 닉네임"
@@ -19,6 +35,9 @@ const LoginForm = () => {
         value={password}
         onChange={(e) => setPassWord(e.target.value)}
       />
+      <button type="submit">로그인</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {success && <p style={{ color: 'green' }}>로그인 성공!</p>}
     </form>
   );
 };
