a62a28973ad5ff1caba403e4122ae2f2e26285b2
hayeon-lee <papepopepe@naver.com>
2025-04-23 15:34:00 +0900

    design: add admin contact

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -7,14 +7,14 @@ const Footer = () => {
     <div
       style={{
         background: '#577aaf',
-        display: 'flex',
-        justifyContent: 'space-between',
       }}
     >
-      <Title level={5}>
-        <span style={{ marginRight: 12 }}>
-          관리자: papepopepe@naver.com / 01066075071
-        </span>
+      <Title level={5} style={{ textAlign: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '3px' }}>
+          <div style={{ color: '#262626' }}>관리자 연락처</div>
+          <div style={{ color: '#262626' }}>papepopepe@naver.com</div>
+          <div style={{ color: '#262626' }}>010-6607-5071</div>
+        </div>
       </Title>
     </div>
   );
