936cfa1a56dd7e649301d1f90febb7b5f4d95acb
Hayeon-Lee <papepopepe@naver.com>
2025-04-23 20:39:48 +0900

    refactor: change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/antd/dist/antd.min.css', // ✅ 혹은
+    './src/styles/**/*.{css,scss}', // ✅ 글로벌 CSS 위치 포함
+  ],
   theme: {
     extend: {
       fontFamily: {
