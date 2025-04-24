3b632d89bafe7134d50b8f991cb1bd264abea00b
hayeon-lee <papepopepe@naver.com>
2025-04-24 09:54:55 +0900

    feat: display bookcard details

--- src/components/book/BookCard.tsx
+++ src/components/book/BookCard.tsx
@@ -1,17 +1,47 @@
-import { Card, Typography } from 'antd';
+import { useState } from 'react';
+import { Card, Typography, Modal, Button } from 'antd';
 import { BookData } from '../../types/book';
 
 const { Title, Text } = Typography;
 
 const BookCard: React.FC<BookData> = ({ title, author, date, memo }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <Card hoverable>
-      <Title level={5}>{title}</Title>
-      <Text>{author}</Text>
-      <br />
-      <Text type="secondary">{date.toLocaleDateString()}</Text>
-      {memo && <p>{memo}</p>}
-    </Card>
+    <>
+      <Card hoverable onClick={() => setIsModalOpen(true)}>
+        <Title level={5}>{title}</Title>
+        <Text>{author}</Text>
+        <br />
+        <Text type="secondary">{date.toLocaleDateString()}</Text>
+        {memo && <p>{memo}</p>}
+      </Card>
+
+      <Modal
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={[
+          <Button key="edit" type="primary">
+            수정
+          </Button>,
+        ]}
+        title="책 정보"
+      >
+        <p>
+          <strong>제목:</strong> {title}
+        </p>
+        <p>
+          <strong>저자:</strong> {author}
+        </p>
+        <p>
+          <strong>날짜:</strong> {date.toLocaleDateString()}
+        </p>
+        <p>
+          <strong>메모: </strong>
+          {memo ? memo : '메모한 내용이 없습니다.'}
+        </p>
+      </Modal>
+    </>
   );
 };
 
